abbe7d8d81161aa7621e1c2fdd549d7477a78a1c
guodada <261731963@qq.com>
2017-10-31 00:17:52 +0800

    b

--- hello.py
+++ hello.py
@@ -5,19 +5,33 @@ from flask import make_response
 from flask_script import Manager
 
 from flask_bootstrap import Bootstrap
+
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 manager = Manager(app)
 
 
 @app.route('/')
 def index():
-    return render_tempalte('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(404)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
