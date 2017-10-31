8dff680946063ff47cbe61e03cdbfaa65c96c6b8
guodada <261731963@qq.com>
2017-10-31 23:20:26 +0800

    b

--- hello.py
+++ hello.py
@@ -1,28 +1,50 @@
-from flask import Flask, render_template, url_for, flash, redirect, session
-from flask import request
-from flask import make_response
-
-from flask_script import Manager
-
-from flask_bootstrap import Bootstrap
-
-from flask_wtf import FlaskForm
-from wtforms.validators import Required
-from wtforms import StringField, BooleanField, SubmitField
-
-from flask_moment import Moment
+import os
 from datetime import datetime
 
+from flask import (Flask, flash, make_response, redirect, render_template,
+                   request, session, url_for)
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, StringField, SubmitField
+from wtforms.validators import Required
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'you will never guess'
 
+manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-manager = Manager(app)
+db = SQLAlchemy(app)
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __str__(self):
+        return self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.INTEGER, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __str__(self):
+        return self.username
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
